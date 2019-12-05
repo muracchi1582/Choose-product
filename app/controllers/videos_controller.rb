e590d5c28dfa697b804ab28157fa8e54739757f0
muracchi1582 <masa1582nh@yahoo.co.jp>
2019-12-05 14:50:56 +0900

    投稿ページ作成

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -4,10 +4,23 @@ class VideosController < ApplicationController
   end
 
   def new
+    @video = Video.new
+    @product = Product.new
+    @products = Product.limit(4)
   end
 
   def create
     @video = Video.new(video_params)
+    @product = Product.new
+    @video.save
+    @product.save
+    binding.pry
+    redirect_to root_path
+  end
+
+  def show
+    @videos　=　Video.order(created_at: :DESC).limit(4)
+    @video　=　Video.find(params[:id])
   end
 
   private
